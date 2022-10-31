b72f94ec04cfed24aa5d3ac6f915a7024b9bbe07
jihad <mdjihadislam166@gmail.com>
2022-10-31 21:59:55 +0600

    ji

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+
+//preloder //
+
 // window.addEventListener("load", function () {
 //     const loader = document.querySelector(".loader");
 //     loader.className += " hidden"; // class "loader hidden"
 // });
 
 
-// <!-- Initialize Swiper -->
+//preloder //
+
+// <!-- Initialize Swiper  start-->
 
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
@@ -12,10 +17,6 @@
     slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
-    // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    // },
     breakpoints: {
     0: {
         slidesPerView: 1,
@@ -32,3 +33,31 @@
       prevEl: ".swiper-button-prev",
     },
   });
+// <!-- Initialize Swiper end -->
+
+
+
+
+// counting days to new year
+const countDownDateTime = new Date(2023, 1, 1, 0, 0, 0).getTime();
+const daysValue = document.querySelector("#days");
+const hoursValue = document.querySelector("#hours");
+const minutesValue = document.querySelector("#minutes");
+const secondsValue = document.querySelector("#seconds");
+// run this function every 1000 ms or 1 second
+let x = setInterval(function () {
+ const dateTimeNow = new Date().getTime();
+ let difference = countDownDateTime - dateTimeNow;
+ // calculating time and assigning values
+ daysValue.innerHTML = Math.floor(difference / (1000 * 60 * 60 * 24));
+ hoursValue.innerHTML = Math.floor(
+ (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+ );
+ minutesValue.innerHTML = Math.floor(
+ (difference % (1000 * 60 * 60)) / (1000 * 60)
+ );
+ secondsValue.innerHTML = Math.floor((difference % (1000 * 60)) / 1000);
+ if (difference < 0) {
+ clearInterval(x);
+ }
+}, 1000);
