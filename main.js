a8b547c7f573e46447060aab55e1f5e2fe507599
jihad <mdjihadislam166@gmail.com>
2022-11-01 21:29:55 +0600

    ki

--- main.js
+++ main.js
@@ -61,3 +61,29 @@ let x = setInterval(function () {
  clearInterval(x);
  }
 }, 1000);
+// team section //
+var swiper = new Swiper(".team-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween:20,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      768: {
+          slidesPerView: 2,
+      },
+      991: {
+          slidesPerView: 4,
+      },
+  },
+});
+
+/* <!-- testmonial section start --> */
+
+
+/* <!-- testmonial section start --> */
